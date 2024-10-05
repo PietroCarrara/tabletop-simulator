1cd5ab9a8b601b6284bfd2808ea5cc48422e69de
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2024-10-05 00:18:32 -0300

    Plot and scatter data points

--- scripts/dataset_loader.gd
+++ scripts/dataset_loader.gd
@@ -1,34 +1,51 @@
 extends Node3D
 
-const wine_sphere_scene: PackedScene = preload("res://scenes/wine_sphere.tscn")
-
 @onready var dataset: Array[Dictionary] = load_dataset()
 @onready var wine_spheres: MultiMesh = $wine_spheres.multimesh
 
+@export var axis1: WineProperty
+@export var axis2: WineProperty
+@export var axis3: WineProperty
+@export var axes_length: float
+@export_range(0.01, 2, 0.01) var sphere_scale: float
+
+@onready var sphere_scale_basis = Basis.from_scale(Vector3(sphere_scale, sphere_scale, sphere_scale))
+
 func _ready() -> void:
     wine_spheres.instance_count = self.dataset.size()
+
+    var min_max_values: Dictionary = {}
+    for item in self.dataset:
+        for property in item:
+            var prop_value = item[property]
+            if property not in min_max_values:
+                # Poor man's tuple
+                min_max_values[property] = Vector2(prop_value, prop_value)
+            else:
+                var min = min_max_values[property].x
+                var max = min_max_values[property].y
+                if prop_value < min:
+                    min_max_values[property].x = prop_value
+                if prop_value > max:
+                    min_max_values[property].y = prop_value
+
     for i in range(self.dataset.size()):
+        var axis_x = prop_to_string(axis1)
+        var axis_y = prop_to_string(axis2)
+        var axis_z = prop_to_string(axis3)
+
+        assert(axis_x in self.dataset[i], "Missing axis 1")
+        assert(axis_y in self.dataset[i], "Missing axis 2")
+        assert(axis_z in self.dataset[i], "Missing axis 3")
+
+        var x = (self.dataset[i][axis_x] - min_max_values[axis_x].x) / (min_max_values[axis_x].y - min_max_values[axis_x].x) * axes_length
+        var y = (self.dataset[i][axis_y] - min_max_values[axis_y].x) / (min_max_values[axis_y].y - min_max_values[axis_y].x) * axes_length
+        var z = (self.dataset[i][axis_z] - min_max_values[axis_z].x) / (min_max_values[axis_z].y - min_max_values[axis_z].x) * axes_length
+
         wine_spheres.set_instance_transform(
             i,
-            Transform3D(Basis(), Vector3(i, i, i))
+            Transform3D(sphere_scale_basis, Vector3(x - axes_length/2, y, -z))
         )
-
-        #var wine_sphere: Node = wine_sphere_scene.instantiate().with_data(
-            #wine["Class"],
-            #wine["Alcohol"],
-            #wine["MalicAcid"],
-            #wine["Ash"],
-            #wine["AshAlcalinity"],
-            #wine["Magnesium"],
-            #wine["TotalPhenols"],
-            #wine["Flavanoids"],
-            #wine["NonflavanoidPhenols"],
-            #wine["Proanthocyanins"],
-            #wine["ColorIntensity"],
-            #wine["Hue"],
-            #wine["OD280/OD315"],
-            #wine["Proline"]
-        #)
 
 func load_dataset() -> Array[Dictionary]:
     var file = FileAccess.open("res://Assets/wine.csv", FileAccess.READ)
@@ -54,3 +71,52 @@ func load_dataset() -> Array[Dictionary]:
         dataset.push_back(parsed_item)
 
     return dataset
+
+
+enum WineProperty {
+    Class,
+    Alcohol,
+    MalicAcid,
+    Ash,
+    AshAlcalinity,
+    Magnesium,
+    TotalPhenols,
+    Flavanoids,
+    NonflavanoidPhenols,
+    Proanthocyanins,
+    ColorIntensity,
+    Hue,
+    OD280_OD315,
+    Proline
+}
+
+func prop_to_string(prop: WineProperty):
+    match prop:
+        WineProperty.Class:
+            return "Class"
+        WineProperty.Alcohol:
+            return "Alcohol"
+        WineProperty.MalicAcid:
+            return "MalicAcid"
+        WineProperty.Ash:
+            return "Ash"
+        WineProperty.AshAlcalinity:
+            return "AshAlcalinity"
+        WineProperty.Magnesium:
+            return "Magnesium"
+        WineProperty.TotalPhenols:
+            return "TotalPhenols"
+        WineProperty.Flavanoids:
+            return "Flavanoids"
+        WineProperty.NonflavanoidPhenols:
+            return "NonflavanoidPhenols"
+        WineProperty.Proanthocyanins:
+            return "Proanthocyanins"
+        WineProperty.ColorIntensity:
+            return "ColorIntensity"
+        WineProperty.Hue:
+            return "Hue"
+        WineProperty.OD280_OD315:
+            return "OD280/OD315"
+        WineProperty.Proline:
+            return "Prolin"
